4ec45a77a633fb179b214cbe790cc58dd075eab8
YUDHA GINONG PRATIDINA <dev.yudhaginongpratidina@gmail.com>
2023-11-29 07:10:53 +0700

    Setup React Router DOM + Struktur Folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
+// IMPORT LIBRARY
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './pages/App.jsx'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+// IMPORT STYLE
 import './assets/index.css'
+
+// IMPORT PAGE
+import Home from './pages/Home'
+
+
+// DEFINISIKAN SEMUA URL (ENDPOINT) DAN HALAMANNYA DISINI
+const routes = [
+  {
+    path: '/*',
+    element: <Home />,
+  },
+]
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+        ))}
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
