4c7d02729da3fcaa44b53eb58a678eaa451a173e
YUDHA GINONG PRATIDINA <dev.yudhaginongpratidina@gmail.com>
2023-12-01 05:10:10 +0700

    Auto stash before merge of "main" and "origin/main"

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,12 +10,20 @@ import './assets/index.css'
 import Home from './pages/Home'
 
 
+// IMPORT ERROR PAGE
+import Error404 from './pages/Error404'
+
+
 // DEFINISIKAN SEMUA URL (ENDPOINT) DAN HALAMANNYA DISINI
 const routes = [
   {
-    path: '/*',
+    path: '/',
     element: <Home />,
   },
+  {
+    path: '*',
+    element: <Error404 />,
+  }
 ]
 
 
