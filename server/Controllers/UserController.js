53d1f6d54baa9128edd34308b5bf2c88cf360bf8
YUDHA GINONG PRATIDINA <dev.yudhaginongpratidina@gmail.com>
2023-12-05 09:52:59 +0700

    Update Server

---
+++ server/Controllers/UserController.js
@@ -0,0 +1,21 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+
+
+const createUser = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+
+
+
+
+module.exports = {
+    
+}
--- server/index.js
+++ server/index.js
@@ -1,26 +1,33 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+// IMPORT LIBRARY
+const cors = require('cors')
+const dotenv = require('dotenv')
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
 
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
 
-// CONFIG
+
+
+// INIT
 dotenv.config()
-const app           = express()
-const port          = process.env.PORT || 3000 
-const __filename    = fileURLToPath(import.meta.url);
-const __dirname     = dirname(__filename);
-const staticPath    = join(__dirname, 'public');
+const app = express()
+
+
+
 
 // MIDDLEWARE
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(staticPath));
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+
+// ROUTES
+app.get('/*', (req, res) => res.status(404).json({ message: 'Not Found' }));
+
 
 
 // SERVER LISTEN
-app.listen(port, () =>  console.log(`Server listening on http://localhost:${port}`))
-
+app.listen(process.env.PORT, () =>  console.log(`Server listening on http://localhost:${process.env.PORT}`))
