821588aafbb57d0e405c1fdaf2eff188fd6aa246
YUDHA GINONG PRATIDINA <dev.yudhaginongpratidina@gmail.com>
2023-12-07 20:44:08 +0700

    Integrasi BE -> FE (Register) & Clean Code

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,89 +1,51 @@
-import React, {useState} from "react";
-import Logo from "../assets/icons/logo.svg";
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import Auth from '../components/template/Auth'
+import Input from '../components/elements/Input'
 
 const Login = () => {
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  const Login = async (e) => {
-    e.preventDefault();
-    console.log(`Email : ${email} dan Password : ${password}`);
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const resetMessages = () => {
+    setError('');
+    setSuccess('');
+  };
+
+  const Login = (e) => {
+    try {
+      e.preventDefault();
+      resetMessages();
+
+      if (!email || !password) {
+        return setError("All fields are required");
+      }
+
+      if (email !== "admin@admin" || password !== "kesehatan") {
+        return setError("Wrong email or password");
+      }
+
+      setSuccess("Login successful");
+      
+    } catch (error) {
+    }
   }
 
   return (
-    <div>
-        <div className="flex flex-col items-center justify-center px-6 pt-8 mx-auto md:h-screen pt:mt-0">
-        <Brand url="/" logo={Logo} name="REKAP" />
-        <div className="w-full max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow">
-            <h2 className="text-2xl font-bold text-gray-900">{" "}Sign in to platform{" "}</h2>
-            <form className="mt-8 space-y-6" onSubmit={Login}>
-                <Input 
-                  type="email" 
-                  name="Email" 
-                  placeholder="name@company.com"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)} 
-                />
-                <Input 
-                  type="password" 
-                  name="Password" 
-                  placeholder="••••••••"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)} 
-                />
-                <Button type="submit" name="Login"/>
-                <Auth />
-            </form>
-        </div>
-        </div>
-    </div>
-  );
-};
+    <Auth name="Login" alternative="If you don't have an account ?" alternativeLink="/register" alternativeText="Sign Up" onSubmit={Login}>
+      {error && <p className="text-red-500 text-center">{error}</p>}
+      {success && <p className="text-green-500 text-center">{success}</p>}
 
-export default Login;
-
-const Brand = (props) => {
-  const { url, logo, name } = props;
-  return (
-    <a href={url} className="flex items-center justify-center mb-8 text-2xl font-semibold lg:mb-10 dark:text-white">
-      <img src={logo} className="mr-4 h-11" alt={name} />
-      <span>{name}</span>
-    </a>
-  );
-};
-
-const Input = (props) => {
-  const { type, name, placeholder, value, onChange } = props;
-  return (
-    <div>
-      <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{" "}{name}{" "}</label>
-      <input
-        type={type}
-        name={name}
-        id={name}
-        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        required
-      />
-    </div>
-  );
-};
-
-const Button = (props) => {
-    const {type, name} = props;
-    return (
-        <button type={type} className="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto">{name}</button>
-    )
+      <Input type="email" name="Email" placeholder="name@company.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input type="password" name="Password" placeholder="••••••••" value={password} onChange={(e) => setPassword(e.target.value)} />
+    </Auth>
+  )
 }
 
-const Auth = (props) => {
-    return (
-        <div className="text-sm font-medium text-gray-500"> 
-            {" "} Not registered? {" "}
-            <a href="" className="text-primary-700 hover:underline dark:text-primary-500">{" "}Create account</a>
-        </div>
-    )
-}
+export default Login
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,100 +1,61 @@
-import React, {useState} from "react";
-import Logo from "../assets/icons/logo.svg";
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import Auth from '../components/template/Auth'
+import Input from '../components/elements/Input'
+import axios from 'axios'
 
 const Register = () => {
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [re_Password, setRePassword] = useState("");
+  const navigate = useNavigate();
 
-  const SignUp = async (e) => {
-    e.preventDefault();
-    console.log(`Email : ${email} dan Password : ${password} dan Re-Password : ${re_Password}`);
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [re_password, setRePassword] = useState("")
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const resetMessages = () => {
+    setError('');
+    setSuccess('');
+  };
+
+  const Register = async (e) => {
+    try {
+      e.preventDefault();
+
+      resetMessages();
+
+      const response = await axios.post("http://localhost:4000/api/users", {
+        name        : name,
+        email       : email,
+        password    : password,
+        re_password : re_password
+      });
+
+      if (response) setSuccess(response.data.msg);
+      setTimeout(() => { navigate('/login'); }, 1000);
+      
+    } catch (error) {
+      setError(error.response.data.msg);
+    }
   }
 
   return (
-    <div>
-        <div className="flex flex-col items-center justify-center px-6 pt-8 mx-auto md:h-screen pt:mt-0">
-        <Brand url="/" logo={Logo} name="REKAP" />
-        <div className="w-full max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow dark:bg-gray-800">
-            <h2 className="text-2xl font-bold text-gray-900 dark:text-white">{" "}Sign up to platform{" "}</h2>
-            <form className="mt-8 space-y-6" onSubmit={SignUp}>
-                <Input 
-                  type="email" 
-                  title="Email"  
-                  name="Email" 
-                  placeholder="name@company.com" 
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <Input 
-                  type="password" 
-                  title="Password" 
-                  name="Password" 
-                  placeholder="••••••••"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)} 
-                />
-                <Input 
-                  type="password" 
-                  title="Repeat Password" 
-                  name="re_Password" 
-                  placeholder="••••••••" 
-                  value={re_Password}
-                  onChange={(e) => setRePassword(e.target.value)}
-                />
-                <Button type="submit" name="Create Account"/>
-                <Auth />
-            </form>
-        </div>
-        </div>
-    </div>
-  );
-};
+    <Auth name="Register" alternative="Already have an account?" alternativeLink="/login" alternativeText="Sign in" onSubmit={Register} >
 
-export default Register;
+      {error && <p className="text-red-500 text-center">{error}</p>}
+      {success && <p className="text-green-500 text-center">{success}</p>}
 
-const Brand = (props) => {
-  const { url, logo, name } = props;
-  return (
-    <a href={url} className="flex items-center justify-center mb-8 text-2xl font-semibold lg:mb-10 dark:text-white">
-      <img src={logo} className="mr-4 h-11" alt={name} />
-      <span>{name}</span>
-    </a>
-  );
-};
+      <Input type="text" name="Name" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+      <Input type="email" name="Email" placeholder="name@company.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input type="password" name="Password" placeholder="••••••••" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <Input type="password" name="Re-Password" placeholder="••••••••" value={re_password} onChange={(e) => setRePassword(e.target.value)} />
 
-const Input = (props) => {
-  const { type, name, placeholder, value, onChange } = props;
-  return (
-    <div>
-      <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{" "}{title}{" "}</label>
-      <input
-        type={type}
-        name={name}
-        id={name}
-        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        required
-      />
-    </div>
-  );
-};
+    </Auth>
+  )
 
-const Button = (props) => {
-    const {type, name} = props;
-    return (
-        <button type={type} className="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 sm:w-auto">{name}</button>
-    )
 }
 
-const Auth = (props) => {
-    return (
-        <div className="text-sm font-medium text-gray-500"> 
-            {" "} I have an account? {" "}
-            <a href="/login" className="text-primary-700 hover:underline dark:text-primary-500">{" "}Login</a>
-        </div>
-    )
-}
+export default Register
